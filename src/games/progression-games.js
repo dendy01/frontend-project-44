293541980200b31bf1dc6a275bea1e80f3ed9229
dendy01 <94925140+dendy01@users.noreply.github.com>
2023-07-14 23:22:06 +0300

    Update progression-games.js

--- src/games/progression-games.js
+++ src/games/progression-games.js
@@ -32,7 +32,7 @@ function progression() {
       counter += 1;
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${item}'.`);
-      console.log("Let's try again, Sam!");
+      console.log(`Let's try again, ${name}!`);
       return;
     }
   }
