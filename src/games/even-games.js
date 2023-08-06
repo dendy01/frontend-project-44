2fceb1792fc62bd9a56e68e583879f3799fafc61
dendy01 <biomannumberone@gmail.com>
2023-08-06 18:19:55 +0300

    moved the expression to a separate isEven function

--- src/games/even-games.js
+++ src/games/even-games.js
@@ -3,10 +3,12 @@ import { getRandomValue } from '../utils.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (value) => (value % 2 === 0 ? 'yes' : 'no');
+
 const generateRound = () => {
   const randomValue = getRandomValue(0, 100);
   const question = `Question: ${randomValue}`;
-  const answer = randomValue % 2 === 0 ? 'yes' : 'no';
+  const answer = isEven(randomValue);
 
   return [question, answer];
 };
