209bc6aab6934d417551db555d213b68d824efd7
dendy01 <biomannumberone@gmail.com>
2023-08-08 20:30:25 +0300

    in the progresstion-games file, replaced the getGreatestCommonDivisor function with getGcd with new functionality

--- src/games/gcd-games.js
+++ src/games/gcd-games.js
@@ -3,25 +3,12 @@ import { getRandomValue } from '../utils.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const getGreatestCommonDivisor = (value1, value2) => {
-  let x = value1;
-  let y = value2;
-
-  while (x !== 0 && y !== 0) {
-    if (x > y) {
-      x %= y;
-    } else {
-      y %= x;
-    }
-  }
-
-  return String(x + y);
-};
+const getGcd = (a, b) => ((a % b) ? getGcd(b, a % b) : Math.abs(b));
 
 const generateRound = () => {
   const value1 = getRandomValue(1, 100);
   const value2 = getRandomValue(1, 100);
-  const correctAnswer = getGreatestCommonDivisor(value1, value2);
+  const correctAnswer = getGcd(value1, value2);
   const question = `${value1} ${value2}`;
 
   return [question, correctAnswer];
